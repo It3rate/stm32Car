6bf029746319b9c9f642a002210706c312cd71be
Robin Debreuil <debreuil.robin@gmail.com>
2024-06-05 23:59:55 -0500

    Added mmWave person detector project.

---
+++ mmWave/Core/Inc/EventLoop.h
@@ -0,0 +1,19 @@
+
+#ifndef SRC_EVENTLOOP_H_
+#define SRC_EVENTLOOP_H_
+
+#include <stdint.h>
+#include <stdio.h>
+
+void EventLoopCpp();   // Cpp function to call into main event loop
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    void EventLoopC();  // C function to call into Cpp event loop from main
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SRC_EVENTLOOP_H_ */
---
+++ mmWave/Core/Src/EventLoop.cpp
@@ -0,0 +1,30 @@
+#include "EventLoop.h"
+#include "main.h"
+#include "tim.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "stm32f4xx_hal_tim_ex.h"
+
+uint8_t buf[] = "MMWAVE\r\n";
+GPIO_PinState detectState = GPIO_PIN_RESET;
+
+void EventLoopCpp() {
+	HAL_TIM_Base_Start(&htim2);
+	while (1)
+	{
+		CDC_Transmit_FS(buf, sizeof(buf));
+		HAL_Delay(1000);
+	}
+}
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	detectState = HAL_GPIO_ReadPin(MMW_OUT_GPIO_Port, MMW_OUT_Pin);
+}
+
+
+// Define all C function calls from main.c below
+extern "C" {
+void EventLoopC() {
+	EventLoopCpp();
+}
+}
