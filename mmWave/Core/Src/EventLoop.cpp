eeef4391c61f30fd967f3ae75be1c28a73db7d98
Robin Debreuil <debreuil.robin@gmail.com>
2024-06-06 00:17:40 -0500

    Added timer to mmWave.

--- mmWave/Core/Src/EventLoop.cpp
+++ mmWave/Core/Src/EventLoop.cpp
@@ -5,20 +5,22 @@
 #include "gpio.h"
 #include "stm32f4xx_hal_tim_ex.h"
 
-uint8_t buf[] = "MMWAVE\r\n";
+uint8_t buf[] = "MMWAVE tick\r\n";
 GPIO_PinState detectState = GPIO_PIN_RESET;
 
 void EventLoopCpp() {
-	HAL_TIM_Base_Start(&htim2);
+	HAL_TIM_Base_Start_IT(&htim2);
 	while (1)
 	{
-		CDC_Transmit_FS(buf, sizeof(buf));
-		HAL_Delay(1000);
 	}
 }
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	detectState = HAL_GPIO_ReadPin(MMW_OUT_GPIO_Port, MMW_OUT_Pin);
+}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	CDC_Transmit_FS(buf, sizeof(buf));
 }
 
 
